cf74187a901ff6ab4f6a8f4b4603b84509c57192
Oleg Martynov <fjolne.yngling@gmail.com>
2024-05-05 16:35:18 +0000

    2p

--- scenes/plane.gd
+++ scenes/plane.gd
@@ -1,23 +1,23 @@
 extends CharacterBody2D
 
-
-const SPEED = 200.0
-const ROT_SPEED = 0.1
-const JUMP_VELOCITY = -400.0
+@export var player_id = 0
+@export var SPEED = 200.0
+@export var ROT_SPEED = 0.1
 
 var projectile = preload("res://scenes/projectile.tscn")
 
 func _ready():
+	$AnimatedSprite2D.animation = "p%s" % player_id
 	velocity = Vector2.RIGHT * SPEED
 	rotation = velocity.angle() + PI / 2
 
 func _physics_process(delta):
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("rotate_left_%s" % player_id, "rotate_right_%s" % player_id)
 	if direction:
 		velocity = velocity.rotated(ROT_SPEED * direction)
 		rotation = velocity.angle() + PI / 2
 	
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("fire_%s" % player_id):
 		var p = projectile.instantiate()
 		p.global_position = global_position + -transform.y * 15
 		p.rotation = rotation
