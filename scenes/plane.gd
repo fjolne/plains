3926e3ab57655ef65262cdf0ad4af73bad75602b
Oleg Martynov <fjolne.yngling@gmail.com>
2024-05-05 19:39:43 +0000

    die on hitting the ground

--- scenes/plane.gd
+++ scenes/plane.gd
@@ -3,18 +3,29 @@ extends CharacterBody2D
 @export var player_id = 0
 @export var SPEED = 200.0
 @export var ROT_SPEED = 0.1
+@export var initial_position = Vector2(0, 0.5)
 
 var projectile = preload("res://scenes/projectile.tscn")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func hit():
 	%state.hit(player_id)
+	
+func die():
+	init()
+
+func init():
+	velocity = Vector2.RIGHT * SPEED
+	rotation = velocity.angle() + PI / 2
+	position = initial_position * get_viewport_rect().size
 
 func _ready():
 	$AnimatedSprite2D.animation = "p%s" % player_id
-	velocity = Vector2.RIGHT * SPEED
-	rotation = velocity.angle() + PI / 2
+	init()
 
 func _physics_process(delta):
+	velocity.y += gravity * delta * 0.1
+	
 	var direction = Input.get_axis("rotate_left_%s" % player_id, "rotate_right_%s" % player_id)
 	if direction:
 		velocity = velocity.rotated(ROT_SPEED * direction)
